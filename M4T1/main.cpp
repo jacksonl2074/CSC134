b79add101d0d20e5edd62ce512e5a19b04ad6abb
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2023-02-22 11:22:00 -0500

    Part 2 (Table of Squares)

--- M4T1/main.cpp
+++ M4T1/main.cpp
@@ -21,10 +21,33 @@ void say_hello(int count) {
   return; // void functions, return is optional (implied)
 }
 
+int square (int num) {
+  // input: num, an integer
+  // output: num squared
+  return num*num;  //square
+}
+
 int main() {
   // call a function
   int times = 0;
   cout << "How many times should I say Hello? ";
   cin  >> times;
   say_hello(times);
+
+  // part 2 - table of squares
+  // The world's first "real program" running on a store-program
+  // computer (David Wheeler, Cambridge, May 6, 1949)
+  
+  int i = 0;
+  int max;
+  cout << "How many numbers in the table of squares? " << endl;
+  cin  >> max;
+  cout << "Table of squares from 0 to " << max << endl;
+  cout << "Number" << "\t" << "Square" << endl;
+  cout << "__________________________" << endl;
+  while (i < max) {
+    cout << i << "\t\t" << square(i) << endl;
+    i++; // or i = i +1
+  }
+  
 }
