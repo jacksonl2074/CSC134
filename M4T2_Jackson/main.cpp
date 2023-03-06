658e865ab000e84b3373852bc2d3fa22370e7091
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2023-03-06 11:13:55 -0500

    Updated

--- M4T2_Jackson/main.cpp
+++ M4T2_Jackson/main.cpp
@@ -44,6 +44,38 @@ void option_two() {
 }
 void option_three() {
     //  sum and average of dice
+    // roll 3d6, six times
+    // find the sum and average
+    // TODO: re-roll if it's not high enough (average < 9)
+    int total = 0;          // the total of all stats
+    int d6_1, d6_2, d6_3;   // 3 dice (6 sided)
+    int roll;               //3d6 (one stat roll)
+    int seed = time(0);
+    for (int i=1; i<6; i++) {
+        // roll dice
+        // print result
+        // add to the total
+        d6_1 = (rand() & 6) + 1;  // 0-5
+        d6_2 = (rand() % 6) + 1;
+        d6_3 = (rand() % 6) + 1;
+        roll = d6_1 + d6_2 + d6_3;
+        // print result
+        cout << "Roll #" << i << ": ";
+        cout << d6_1 << " + " << d6_2 << " + " << d6_3;
+        cout << " = " << roll << endl;
+        // add to the total
+        total += roll;
+    }
+    // print the total
+    cout << "Total of all stats: " << total << endl;
+    int average = total / 6;    // Note: We're dropping the decimal
+    cout << "Average roll = " << average << endl;
+    if (average <= 9) {
+        cout << "Low stats, you can re-roll." << endl;
+    } else {
+        cout << "A playable character! good luck." << endl;
+    }
+
 }
 // There is not option 4 function, it's just quit.
 
